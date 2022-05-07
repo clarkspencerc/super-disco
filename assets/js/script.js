b81265b8c654b9ad00faf0e6f4741261e4cae4dc
clarkspencerc <clarkspencerc@gmail.com>
2022-05-07 15:24:04 -0600

    classes for past present and future & accounting for AM/PM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var currentDayEl = document.querySelector("#currentDay");
+var workingHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
 //grab current date from moment.js and have it display on currentDay id 
 
@@ -16,14 +17,35 @@ $(".saveBtn").on("click", function(){
     $("#10 .description").val(localStorage.getItem("10"));
     $("#11 .description").val(localStorage.getItem("11"));
     $("#12 .description").val(localStorage.getItem("12")); 
-    $("#1 .description").val(localStorage.getItem("1")); 
-    $("#2 .description").val(localStorage.getItem("2")); 
-    $("#3 .description").val(localStorage.getItem("3")); 
-    $("#4 .description").val(localStorage.getItem("4")); 
-    $("#5 .description").val(localStorage.getItem("5")); 
+    $("#13 .description").val(localStorage.getItem("13")); 
+    $("#14 .description").val(localStorage.getItem("14")); 
+    $("#15 .description").val(localStorage.getItem("15")); 
+    $("#16 .description").val(localStorage.getItem("16")); 
+    $("#17 .description").val(localStorage.getItem("17")); 
 
 // get the current hour from moment.js with which we can compare the div IDs to add classes: past, present, or future
 
+var hour = moment().format('H'); 
+console.log(hour); 
     // create a conditional that targets the time block in the HTML, grab the timeblock id and then add
     // conditional that if id is > current time then add class for color: pst 
 
+    for( i = 0; i < workingHours.length; i++){
+
+        if( workingHours[i] < hour ){
+            // add past class by  getelement by id id = workingHours[i]
+            var updatePast = document.getElementById(workingHours[i]);
+            updatePast.classList.add("past");
+            // need to account for 2 < 10
+        }
+        else if( workingHours[i] == hour){
+            // add current class 
+            var updateCurrent = document.getElementById(workingHours[i]);
+            updateCurrent.classList.add("present");
+        }
+        else if (workingHours[i] > hour){
+            var updateFuture = document.getElementById(workingHours[i]);
+            updateFuture.classList.add("future");
+            // add future class 
+        }
+    }
